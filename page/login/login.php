f20df3d254142216b7e5aab0c9cd1e0fede42ea9
jms1132@naver.com <54921451+jms1132@users.noreply.github.com>
2020-02-14 17:10:11 +0900

    카카오톡 로그인 시도

--- page/login/login.php
+++ page/login/login.php
@@ -1,207 +1,48 @@
 <html lang="ko">
 <head>
 <script type="text/javascript" src="https://static.nid.naver.com/js/naveridlogin_js_sdk_2.0.0.js" charset="utf-8"></script>
-
 <script src="//developers.kakao.com/sdk/js/kakao.min.js"></script>
 
-<style rel="stylesheet">
-*{padding: 0;
-margin:0;}
-body {
-   font: 13px/20px 'Helvetica Neue', Helvetica, Arial, sans-serif;
-   color: #333333;
-   /* background: #596778; */
-}
-
-.signUp {
-   position: relative;
-   margin: 10px auto;
-   width: 280px;
-   padding: 33px 25px 29px;
-   background: #FFFFFF;
-   border-bottom: 1px solid #C4C4C4;
-   border-radius: 5px;
-   -webkit-box-shadow: 0 1px 5px rgba(0, 0, 0, 0.25);
-   box-shadow: 0 1px 5px rgba(0, 0, 0, 0.25);
-}
-
-.signUp:before,
-.signUp:after {
-   content: '';
-   position: absolute;
-   bottom: 1px;
-   left: 0;
-   right: 0;
-   height: 10px;
-   background: inherit;
-   border-bottom: 1px solid #D2D2D2;
-   border-radius: 4px;
-}
-
-.signUp:after {
-   bottom: 3px;
-   border-color: #DCDCDC;
-}
-
-/* .signUpTitle {
-   margin: -25px -25px 25px;
-   padding: 15px 25px;
-   line-height: 35px;
-   font-size: 26px;
-   font-weight: 300;
-   color: #777;
-   text-align: center;
-   text-shadow: 0 1px rgba(255, 255, 255, 0.75);
-   background: #F7F7F7;
-} */
-
-.signUpTitle:before {
-   content: '';
-   position: absolute;
-   top: 0;
-   left: 0;
-   right: 0;
-   height: 8px;
-   background: #C4E17F;
-   border-radius: 5px 5px 0 0;
-   background-image: -webkit-linear-gradient(left, #C4E17F, #C4E17F 12.5%, #F7FDCA 12.5%, #F7FDCA 25%, #FECF71 25%, #FECF71 37.5%, #F0776C 37.5%, #F0776C 50%, #DB9DBE 50%, #db9CBE 62.5%, #C49CDE 62.5%, #C49CDE 75%, #669AE1 75%, #669AE1 87.5%, #62C2E4 87.5%, #62C2E4);
-   background-image: -moz-linear-gradient(left, #c4e17f, #C4E17F 12.5%, #F7FDCA 12.5%, #F7FDCA 25%, #FECF71 25%, #FECF71 37.5%, #F0776C 37.5%, #F0776C 50%, #DB9DBE 50%, #DB9CBE 62.5%, #C49CDE 62.5%, #C49CDE 75%, #669AE1 75%, #669AE1 87.5%, #62C2E4 87.5%, #62C2E4);
-   background-image: -o-linear-gradient(left, #C4E17F, #C4E17F 12.5%, #F7FDCC 12.5%, #F7FDCA 25%, #FECF71 25%, #FECF71 37.5%, #F0776C 37.5%, #F0776C 50%, #DB9DBE 50%, #DB9DBE 62.5%, #C49CDE 62.5%, #C49CDE 75%, #669AE1 75%, #669AE1 87.5%, #62C2E4 87.5%, #62C2E4);
-   background-image: linear-gradient(to right, #C4E17F, #C4E17F 12.5%, #F7FDCA 12.5%, #F7FDCA 25%, #FECF71 25%, #FECF71 37.5%, #F0776C 37.5%, #F0776C 50%, #DB9DBE 50%, #DB9CBE 62.5%, #c49cde 62.5%, #C49CDE 75%, #669AE1 75%, #669AE1 87.5%, #62c2e4 87.5%, #62C2E4);
-}
-
-input {
-   font-family: inherit;
-   color: inherit;
-   -webkit-box-sizing: border-box;
-   -moz-box-sizing: border-box;
-   box-sizing: border-box;
-}
-
-.signUpInput {
-   width: 100%;
-   height: 50px;
-   margin-bottom: 25px;
-   padding: 0 15px 2px;
-   font-size: 17px;
-   background: white;
-   border: 2px solid #EBEBEB;
-   border-radius: 4px;
-   -webkit-box-shadow: inset 0 -2px #EBEBEB;
-   box-shadow: inset 0 -2px #EBEBEB;
-}
-
-.signUpInput:focus {
-   border-color: #62C2E4;
-   outline: none;
-   -webkit-box-shadow: inset 0 -2px #62C2E4;
-   box-shadow: inset 0 -2px #62C2E4;
-}
-
-.lt-ie9 .signUpInput {
-   line-height: 48px;
-}
-
-.signUpButton {
-   position: relative;
-   vertical-align: top;
-   width: 100%;
-   height: 54px;
-   padding: 0;
-   font-size: 22px;
-   color: white;
-   text-align: center;
-   text-shadow: 0 1px 2px rgba(0, 0, 0, 0.25);
-   background: #F0776C;
-   border: 0;
-   border-bottom: 2px solid #D76B60;
-   border-radius: 5px;
-   cursor: pointer;
-   -webkit-box-shadow: inset 0 -2px #D76B60;
-   box-shadow: inset 0 -2px #D76B60;
-}
-
-.signUpButton:active {
-   top: 1px;
-   outline: none;
-   -webkit-box-shadow: none;
-   box-shadow: none;
-}
-
-:-moz-placeholder {
-   color: #AAAAAA;
-   font-weight: 300;
-}
-
-::-moz-placeholder {
-   color: #AAAAAA;
-   opacity: 1;
-   font-weight: 300;
-}
-
-::-webkit-input-placeholder {
-   color: #AAAAAA;
-   font-weight: 300;
-}
-
-:-ms-input-placeholder {
-   color: #AAAAAA;
-   font-weight: 300;
-}
-
-::-moz-focus-inner {
-   border: 0;
-   padding: 0;
-}
-</style>
+<link rel="stylesheet" href="../../css/login/login.css">
 </head>
+
 <body>
-
-
-
-
-
 	<form class="signUp" id="signupForm">
 	   <h1 class="signUpTitle">Sign up in second</h1>
 	   <input type="text" class="signUpInput" placeholder="Type your username" autofocus required>
 	   <input type="password" class="signUpInput" placeholder="Choose a password" required>
 	   <input type="submit" value="Sign me up!" class="signUpButton">
+
 		 <div id="naverIdLogin"></div>
-
      <a id="kakao-login-btn"></a>
-     <a href="http://developers.kakao.com/logout"></a>
-     <script type='text/javascript'>
-       //<![CDATA[
-         // 사용할 앱의 JavaScript 키를 설정해 주세요.
-         Kakao.init('d4c9f81c1dd87d7457ae9ba104e93a3d');
-         // 카카오 로그인 버튼을 생성합니다.
-         Kakao.Auth.createLoginButton({
-           container: '#kakao-login-btn',
-           //담겨져 나오는 카카오톡 정보값 확인
-           // success: function(authObj) {
-           //   Kakao.API.request({
-           //     url:'/v2/user/me',
-           //     success: function(res){
-           //       alert(JSON.stringify(res));
-           //       alert(JSON.stringify(authObj));
-           //       console.log(res.id);
-           //       console.log(res.kaccount_email);
-           //     }
-           //
-           //   })
-           // },
-           fail: function(err) {
-              alert(JSON.stringify(err));
-           }
-         });
+          <a href="http://developers.kakao.com/logout"></a>
+          <script type='text/javascript'>
+            //<![CDATA[
+              // 사용할 앱의 JavaScript 키를 설정해 주세요.
+              Kakao.init('d4c9f81c1dd87d7457ae9ba104e93a3d');
+              // 카카오 로그인 버튼을 생성합니다.
+              Kakao.Auth.createLoginButton({
+                container: '#kakao-login-btn',
+                // 담겨져 나오는 카카오톡 정보값 확인s
+                success: function(authObj) {
+                  Kakao.API.request({
+                    url:'/v2/user/me',
+                    success: function(res){
+                      alert(JSON.stringify(res));
+                      alert(JSON.stringify(authObj));
+                      console.log(res.id);
+                      console.log(res.kaccount_email);
+                    }
 
-     </script>
-	</form>
+                  })
+                },
+                fail: function(err) {
+                   alert(JSON.stringify(err));
+                }
+              });
 
-
-
-
-<!-- //네이버아이디로로그인 버튼 노출 영역 -->
+          </script>
+    	</form>
 
 <!-- 네이버아디디로로그인 초기화 Script -->
 <script type="text/javascript">
@@ -218,9 +59,6 @@ input {
 	naverLogin.init();
 
 </script>
-<!-- // 네이버아이디로로그인 초기화 Script -->
-
-<!-- 네이버 API 예제 - 회원프로필 조회 -->
 
 </body>
 </html>
