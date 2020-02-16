9a4b4df954de0bf17b6bd1bdfbba72984fbf0004
tlawogus11106 <tlawogus11106@gmail.com>
2020-02-16 17:33:41 +0900

    상대경로 경로 수정

--- db/db_connector_main.php
+++ db/db_connector_main.php
@@ -1,6 +1,6 @@
 <?php
-    include "./db_connector.php";
-    include "./create_table.php";
+    include "../../db/db_connector.php";
+    include "../../db/create_table.php";
 
     createTable($connect, 'member', DB_NAME);
     createTable($connect, 'message', DB_NAME);
