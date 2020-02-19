70b9f02cdd9686f306f26a924334e6778e3732d6
tlawogus11106 <tlawogus11106@gmail.com>
2020-02-19 10:16:15 +0900

    컨텐트 내용 추가

--- lib/common_page/content.php
+++ lib/common_page/content.php
@@ -12,21 +12,113 @@
         <span class="reply"><img src="" alt="">댓글 "15개"</span>
       </div>
     </div>
+  </div>
+  <!-- /.container -->
+  <div class="container">
     <!-- Heading Row -->
     <div class="row align-items-center my-5">
-       <div class="col-lg-7 no-flex">
-         <img class="img-fluid rounded mb-4 mb-lg-0" src="http://placehold.it/900x400" alt="">
-       </div>
-       <div class="col-lg-5 no-flex">
-         <h1 class="font-weight-light">Business Name or Tagline</h1>
-         <p>This is a template that is great for small businesses. It doesn't have too much fancy flare to it, but it makes a great use of the standard Bootstrap core components. Feel free to use this template for any project you want!</p>
-         <a class="btn btn-primary" href="../../page/board/board_widen.php">Call to Action!</a>
-         <span class="reply"><img src="" alt="">댓글 "15개"</span>
-       </div>
-     </div>
-   </div>
-   <!-- /.container -->
+      <div class="col-lg-7 no-flex">
+        <img class="img-fluid rounded mb-4 mb-lg-0" src="http://placehold.it/900x400" alt="">
+      </div>
+      <div class="col-lg-5 no-flex">
+        <h1 class="font-weight-light">Business Name or Tagline</h1>
+        <p>This is a template that is great for small businesses. It doesn't have too much fancy flare to it, but it makes a great use of the standard Bootstrap core components. Feel free to use this template for any project you want!</p>
+        <a class="btn btn-primary" href="../../page/board/board_widen.php">Call to Action!</a>
+        <span class="reply"><img src="" alt="">댓글 "15개"</span>
+      </div>
+    </div>
   </div>
+  <!-- /.container -->
+  <div class="container">
+    <!-- Heading Row -->
+    <div class="row align-items-center my-5">
+      <div class="col-lg-7 no-flex">
+        <img class="img-fluid rounded mb-4 mb-lg-0" src="http://placehold.it/900x400" alt="">
+      </div>
+      <div class="col-lg-5 no-flex">
+        <h1 class="font-weight-light">Business Name or Tagline</h1>
+        <p>This is a template that is great for small businesses. It doesn't have too much fancy flare to it, but it makes a great use of the standard Bootstrap core components. Feel free to use this template for any project you want!</p>
+        <a class="btn btn-primary" href="../../page/board/board_widen.php">Call to Action!</a>
+        <span class="reply"><img src="" alt="">댓글 "15개"</span>
+      </div>
+    </div>
+  </div>
+  <!-- /.container -->
+  <div class="container">
+    <!-- Heading Row -->
+    <div class="row align-items-center my-5">
+      <div class="col-lg-7 no-flex">
+        <img class="img-fluid rounded mb-4 mb-lg-0" src="http://placehold.it/900x400" alt="">
+      </div>
+      <div class="col-lg-5 no-flex">
+        <h1 class="font-weight-light">Business Name or Tagline</h1>
+        <p>This is a template that is great for small businesses. It doesn't have too much fancy flare to it, but it makes a great use of the standard Bootstrap core components. Feel free to use this template for any project you want!</p>
+        <a class="btn btn-primary" href="../../page/board/board_widen.php">Call to Action!</a>
+        <span class="reply"><img src="" alt="">댓글 "15개"</span>
+      </div>
+    </div>
+  </div>
+  <!-- /.container -->
+  <div class="container">
+    <!-- Heading Row -->
+    <div class="row align-items-center my-5">
+      <div class="col-lg-7 no-flex">
+        <img class="img-fluid rounded mb-4 mb-lg-0" src="http://placehold.it/900x400" alt="">
+      </div>
+      <div class="col-lg-5 no-flex">
+        <h1 class="font-weight-light">Business Name or Tagline</h1>
+        <p>This is a template that is great for small businesses. It doesn't have too much fancy flare to it, but it makes a great use of the standard Bootstrap core components. Feel free to use this template for any project you want!</p>
+        <a class="btn btn-primary" href="../../page/board/board_widen.php">Call to Action!</a>
+        <span class="reply"><img src="" alt="">댓글 "15개"</span>
+      </div>
+    </div>
+  </div>
+  <!-- /.container -->
+  <div class="container">
+    <!-- Heading Row -->
+    <div class="row align-items-center my-5">
+      <div class="col-lg-7 no-flex">
+        <img class="img-fluid rounded mb-4 mb-lg-0" src="http://placehold.it/900x400" alt="">
+      </div>
+      <div class="col-lg-5 no-flex">
+        <h1 class="font-weight-light">Business Name or Tagline</h1>
+        <p>This is a template that is great for small businesses. It doesn't have too much fancy flare to it, but it makes a great use of the standard Bootstrap core components. Feel free to use this template for any project you want!</p>
+        <a class="btn btn-primary" href="../../page/board/board_widen.php">Call to Action!</a>
+        <span class="reply"><img src="" alt="">댓글 "15개"</span>
+      </div>
+    </div>
+  </div>
+  <!-- /.container -->
+  <div class="container">
+    <!-- Heading Row -->
+    <div class="row align-items-center my-5">
+      <div class="col-lg-7 no-flex">
+        <img class="img-fluid rounded mb-4 mb-lg-0" src="http://placehold.it/900x400" alt="">
+      </div>
+      <div class="col-lg-5 no-flex">
+        <h1 class="font-weight-light">Business Name or Tagline</h1>
+        <p>This is a template that is great for small businesses. It doesn't have too much fancy flare to it, but it makes a great use of the standard Bootstrap core components. Feel free to use this template for any project you want!</p>
+        <a class="btn btn-primary" href="../../page/board/board_widen.php">Call to Action!</a>
+        <span class="reply"><img src="" alt="">댓글 "15개"</span>
+      </div>
+    </div>
+  </div>
+  <!-- /.container -->
+  <div class="container">
+    <!-- Heading Row -->
+    <div class="row align-items-center my-5">
+      <div class="col-lg-7 no-flex">
+        <img class="img-fluid rounded mb-4 mb-lg-0" src="http://placehold.it/900x400" alt="">
+      </div>
+      <div class="col-lg-5 no-flex">
+        <h1 class="font-weight-light">Business Name or Tagline</h1>
+        <p>This is a template that is great for small businesses. It doesn't have too much fancy flare to it, but it makes a great use of the standard Bootstrap core components. Feel free to use this template for any project you want!</p>
+        <a class="btn btn-primary" href="../../page/board/board_widen.php">Call to Action!</a>
+        <span class="reply"><img src="" alt="">댓글 "15개"</span>
+      </div>
+    </div>
+  </div>
+  <!-- /.container -->
   <div id="pop_up">
     <div id="pop_box" onmouseleave="pop_down()">
       <ul>
