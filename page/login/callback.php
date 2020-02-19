0f9214057a1ebbdd54241a1cf2c5dccb4354c2b3
jms1132@naver.com <54921451+jms1132@users.noreply.github.com>
2020-02-19 09:24:02 +0900

    네이버/카카오 로그인 수정

--- page/login/callback.php
+++ page/login/callback.php
@@ -1,61 +1,37 @@
-<!DOCTYPE html>
-<html lang="kr">
-
+<!doctype html>
+<html lang="ko">
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>NaverLoginSDK</title>
+<script type="text/javascript" src="https://static.nid.naver.com/js/naverLogin_implicit-1.0.3.js" charset="utf-8"></script>
+<script type="text/javascript" src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
 </head>
-
 <body>
-	<!-- 네이버 로그인 callback -->
+<script type="text/javascript">
+  var naver_id_login = new naver_id_login("txJsAHBUQ68ptqMzm_5I", "http://localhost/team_project/page/login/callback.php");
+  // 접근 토큰 값 출력
+  alert(naver_id_login.oauthParams.access_token);
+  // 네이버 사용자 프로필 조회
+  naver_id_login.get_naver_userprofile("naverSignInCallback()");
+  // 네이버 사용자 프로필 조회 이후 프로필 정보를 처리할 callback function
+  function naverSignInCallback() {
+    alert(naver_id_login.getProfileData('email'));
+		alert(naver_id_login.getProfileData('name'));
 
-	callback 처리중입니다. 이 페이지에서는 callback을 처리하고 바로 main으로 redirect하기때문에 이 메시지가 보이면 안됩니다.
-
-	<!-- (1) LoginWithNaverId Javscript SDK -->
-	<script type="text/javascript" src="https://static.nid.naver.com/js/naveridlogin_js_sdk_2.0.0.js" charset="utf-8"></script>
-
-
-	<!-- (2) LoginWithNaverId Javscript 설정 정보 및 초기화 -->
-	<script>
-		var naverLogin = new naver.LoginWithNaverId(
-			{
-				clientId: "txJsAHBUQ68ptqMzm_5I",
-				callbackUrl: "{YOUR_REDIRECT_URL}",
-				isPopup: false,
-				callbackHandle: true
-				/* callback 페이지가 분리되었을 경우에 callback 페이지에서는 callback처리를 해줄수 있도록 설정합니다. */
-			}
-		);
-
-		/* (3) 네아로 로그인 정보를 초기화하기 위하여 init을 호출 */
-		naverLogin.init();
-
-		/* (4) Callback의 처리. 정상적으로 Callback 처리가 완료될 경우 main page로 redirect(또는 Popup close) */
-		window.addEventListener('load', function () {
-			naverLogin.getLoginStatus(function (status) {
-				if (status) {
-					/* (5) 필수적으로 받아야하는 프로필 정보가 있다면 callback처리 시점에 체크 */
-					var email = naverLogin.user.getEmail();
-					if( email == undefined || email == null) {
-						alert("이메일은 필수정보입니다. 정보제공을 동의해주세요.");
-						/* (5-1) 사용자 정보 재동의를 위하여 다시 네아로 동의페이지로 이동함 */
-						naverLogin.reprompt();
-						return;
-					}
-
-					window.location.replace("http://" + window.location.hostname +
-                            ( (location.port==""||location.port==undefined)?"":":" + location.port) + "/team_project/page/login/main.php");
-				                                                    /* 인증이 완료된후 /sample/main.html 페이지로 이동하라는것이다. 본인 페이로 수정해야한다. */
-                 } else {
-					console.log("callback 처리에 실패하였습니다.");
+		$.ajax({
+			type: "POST",
+			url: "http://localhost/team_project/page/login/create_session.php",
+			data: "name="+naver_id_login.getProfileData('name'),
+			success:function(data) {
+				if(data === "name_error") {
+					alert('NAME_ERROR');
+				} else {
+					alert(data+' 님 환영합니다.');
+					opener.parent.location.reload();
+					window.close();
 				}
-			});
+			}
 		});
-	</script>
 
-
+  }
+</script>
 </body>
-
 </html>
--- page/login/login_and_signup.php
+++ page/login/login_and_signup.php
@@ -3,15 +3,15 @@
   <head>
     <meta charset="utf-8">
     <title></title>
-    <script type="text/javascript" src="https://static.nid.naver.com/js/naveridlogin_js_sdk_2.0.0.js" charset="utf-8"></script>
+    <script type="text/javascript" src="https://static.nid.naver.com/js/naverLogin_implicit-1.0.3.js" charset="utf-8"></script>
+    <script type="text/javascript" src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
     <script src="//developers.kakao.com/sdk/js/kakao.min.js"></script>
-    <script src="../../js/login/vendor/jquery/jquery.min.js"></script>
     <script src="../../js//login/signup.js"></script>
-
-
-    <link rel="stylesheet" href="../../css/login/login.css">
+    <script src="../../js//login/login.js"></script>
     <script type="text/javascript">
-    $(document).ready(function(){
+
+      $(document).ready(function(){
+        
         $('ul.tabs li').click(function(){
           var tab_id = $(this).attr('data-tab');
 
@@ -21,241 +21,27 @@
           $(this).addClass('current');
           $("#"+tab_id).addClass('current');
         });
+
+        function done(){
+          document.login_form.submit();
+        }
+        function newLogin(){
+               Kakao.Auth.loginForm({
+                 success: function(authObj) {
+                   alert(JSON.stringify(authObj));
+                 },
+                 fail: function(err) {
+                   alert(JSON.stringify(err));
+                 }
+              })
+             }
     });
     </script>
     <link rel="stylesheet" href="../../css/login/login.css">
-    <style media="screen">
-    body{
-margin-top: 50px;
-font-family: 'Trebuchet MS', serif;
-line-height: 1.6
-}
-.container{
-width: 500px;
-margin: 0 auto;
-}
-
-ul.tabs{
-margin: 0px;
-padding: 0px;
-list-style: none;
-}
-ul.tabs li{
-background: none;
-color: #222;
-display: inline-block;
-padding: 10px 15px;
-cursor: pointer;
-}
-
-ul.tabs li.current{
-
-color: #222;
-}
-
-.tab-content{
-display: none;
-
-padding: 15px;
-}
-
-.tab-content.current{
-display: inherit;
-}
-
-/* 회원가입 */
-input {
-  box-sizing: border-box;
-  border: 0.5px solid lightgray;
-  border-radius: 2px;
-}
-#member_main_content {
-  width: 400px;
-  margin: 0 auto;
-}
-#title_member {
-  text-align: center;
-  margin-top: 40px;
-  margin-bottom: 20px;
-}
-#member_form input {
-  width: 400px;
-  height: 50px;
-  margin-bottom: 10px;
-  box-sizing: border-box;
-}
-#phone {
-  width: 400px;
-  height: 40px;
-  margin-bottom: 10px;
-  box-sizing: border-box;
-}
-#phone #phone_input {
-  display: inline-block;
-  float: left;
-  box-sizing: border-box;
-}
-#phone select {
-  width: 60px;
-  height: 40px;
-  margin-bottom: 10px;
-  box-sizing: border-box;
-}
-
-#phone input {
-  width: 103px;
-  height: 40px;
-  margin-bottom: 10px;
-  box-sizing: border-box;
-}
-#email {
-  margin-bottom: 70px;
-}
-
-#email #email_one {
-  width: 175px;
-  height: px;
-  margin-bottom: 10px;
-  box-sizing: border-box;
-}
-
-#email input {
-  width: 97px;
-  height: 40px;
-  margin-bottom: 10px;
-  box-sizing: border-box;
-}
-
-#email select {
-  width: 95px;
-  height: 40px;
-  margin-bottom: 10px;
-  box-sizing: border-box;
-}
-
-#email_certification {
-  display: inline-block;
-  float: left;
-  width: 95px;
-  height: 40px;
-  margin-bottom: 10px;
-  box-sizing: border-box;
-  border: 1px solid #1C1C1C;
-  border-radius: 2px;
-}
-
-#email_certification a {
-  text-decoration: none;
-  color: #1C1C1C;
-}
-
-#email_certification a p {
-  width: 95px;
-  height: 33px;
-  text-align: center;
-  font-size: 13px;
-  margin: 0px;
-  padding: 7px 0px 0px 0px;
-}
-
-#email_certification_check input {
-  display: inline-block;
-  float: left;
-  width: 175px;
-  height: 40px;
-  margin-bottom: 10px;
-  margin-right: 7px;
-  box-sizing: border-box;
-}
-
-#email_certification_check_button {
-  display: inline-block;
-  float: left;
-  width: 112px;
-  height: 40px;
-  margin-bottom: 10px;
-  margin-right: 7px;
-  box-sizing: border-box;
-  background-color: #0B3861;
-  border-radius: 2px;
-}
-
-#email_certification_check_button a {
-  text-decoration: none;
-  color: #1C1C1C;
-}
-
-#email_certification_check_button a p {
-  width: 95px;
-  height: 33px;
-  text-align: center;
-  font-size: 13px;
-  color: white;
-  margin: 0px;
-  padding: 9px 0px 0px 6px;
-}
-#button {
-  width: 400px;
-  height: 40px;
-  box-sizing: border-box;
-  margin: 0 auto;
-  margin-bottom: 40px;
-}
-#cancel_btn {
-  display: inline-block;
-  float: left;
-  width: 130px;
-  height: 40px;
-  margin-bottom: 10px;
-  margin-left: 60px;
-  box-sizing: border-box;
-  background-color: gray;
-  border-radius: 2px;
-}
-#cancel_btn a {
-  text-decoration: none;
-}
-#cancel_btn a p {
-  display: inline-block;
-  width: 130px;
-  height: 38px;
-  text-align: center;
-  font-size: 13px;
-  color: white;
-  margin: 0px;
-  padding: 9px 0px 0px 0px;
-}
-#signup_btn {
-  display: inline-block;
-  float: left;
-  width: 130px;
-  height: 40px;
-  margin-bottom: 10px;
-  margin-left: 20px;
-  box-sizing: border-box;
-  background-color: #F23005;
-  border-radius: 2px;
-}
-#signup_btn a {
-  text-decoration: none;
-  color: #1C1C1C;
-}
-
-#signup_btn a p {
-  display: inline-block;
-  width: 130px;
-  height: 38px;
-  text-align: center;
-  font-size: 13px;
-  color: white;
-  margin: 0px;
-  padding: 9px 0px 0px 0px;
-}
-    </style>
+    <link rel="stylesheet" href="../../css/login/signup.css">
   </head>
   <body>
     <div class="container">
-
   	<ul class="tabs">
   		<li class="tab-link current" data-tab="signup">회원가입</li>
   		<li class="tab-link" data-tab="login">로그인</li>
@@ -284,28 +70,13 @@ input {
                 <input type="number" name="phone_two" maxlength="4" placeholder=" 0000 " oninput="numberMaxLength(this);"> -
                 <input type="number" name="phone_three" maxlength="4" placeholder=" 0000 " oninput="numberMaxLength(this);">
               </div>
+              <div id="email_certification">
+                <a href="#" onclick="">
+                  <p>인증 요청</p>
+                </a>
+              </div>
             </div>
             <div id="email">
-              <div id="email_input">
-                <input type="text" id="email_one" name="email_one" placeholder=" 이메일 입력 "> @
-                <input type="text" name="email_two" id="email_two">
-                <select name="email_option" id="email_option">
-                  <option value="gmail.com">gmail.com</option>
-                  <option value="naver.com">naver.com</option>
-                  <option value="daum.net">daum.net</option>
-                  <option value="">직접 입력</option>
-                </select> <br>
-                <script type="text/javascript">
-                  $(function(){
-                    var tag_mail=$('#email_two');
-                    $('#email_option').change(function(){
-                      var element=$(this).find('option:selected');
-                      var myTag=element.attr('value');
-                      tag_mail.val(myTag);
-                    });
-                  });
-                </script>
-              </div>
               <div id="email_certification_check">
                 <input type="text" name="" placeholder=" 인증 번호 입력 ">
                 <div id="email_certification_check_button">
@@ -313,14 +84,8 @@ input {
                     <p>확 인</p>
                   </a>
                 </div>
-                <div id="email_certification">
-                  <a href="#" onclick="">
-                    <p>인증 요청</p>
-                  </a>
-                </div>
               </div>
             </div>
-
             <div id="button">
               <div id="cancel_btn">
                 <a href="#" onclick="reset_form()">
@@ -336,21 +101,20 @@ input {
           </form>
         </div>
       </div>
-
   	</div>
   	<div id="login" class="tab-content">
-      <form class="signUp" id="signupForm">
+      <form method="POST" class="signUp" name="login_form" action="./login_session.php">
          <h1 class="signUpTitle">Sign up in second</h1>
-         <input type="text" class="signUpInput" placeholder="Type your username" autofocus required>
-         <input type="password" class="signUpInput" placeholder="Choose a password" required>
-         <input type="submit" value="Sign me up!" class="signUpButton">
-
-         <div id="naverIdLogin"><a id="naverIdLogin_loginButton" href="#" role="button"></a></div>
-         <a id="kakao-login-btn"></a>
+         <input type="text" name="id" class="signUpInput" placeholder="Type your username" autofocus required>
+         <input type="password" name="password" class="signUpInput" placeholder="Choose a password" required>
+         <input type="button" value="로그인" class="signUpButton" onclick="done()">
+         <div id="naver_id_login"></div>
+         <!-- <input type="button" id="kakao-login-btn" onclick="newLogin()"> -->
+         <a id="kakao-login-btn" onclick="newLogin()"></a>
       </form>
 
-      <!-- 카카오톡 로그인 -->
-      <script type='text/javascript'>
+    <!-- ===================================카카오 로그인========================================= -->
+          <script type='text/javascript'>
 				//<![CDATA[
 					// 사용할 앱의 JavaScript 키를 설정해 주세요.
 					Kakao.init('d4c9f81c1dd87d7457ae9ba104e93a3d');
@@ -364,10 +128,23 @@ input {
 								success: function(res){
 									// alert(JSON.stringify(res));
 									// alert(JSON.stringify(authObj)); //res에 담겨있는 json값을 모두 확인가능
+
 									alert(res.properties.nickname+'님 환영합니다.');
-
+                  $.ajax({
+                    type: "POST",
+                    url: "http://localhost/team_project/page/login/create_session.php",
+                    data: "name="+res.properties.nickname,
+                    success:function(data) {
+                      if(data === "name_error") {
+                        alert('NAME_ERROR');
+                      } else {
+                        alert(data+' 님 환영합니다.');
+                        opener.parent.location.reload();
+                        window.close();
+                      }
+                    }
+                  });
 								}
-
 							});
 						},
 						fail: function(err) {
@@ -376,29 +153,16 @@ input {
 					});
 			</script>
 
-<!-- 네이버아이디로로그인 초기화 Script -->
+
+<!-- ========================================네이버 로그인========================================== -->
 <script type="text/javascript">
-	var naverLogin = new naver.LoginWithNaverId(
-		{
-			clientId: "txJsAHBUQ68ptqMzm_5I",
-			callbackUrl: "http://localhost/team_project/page/login/callback.php",
-			isPopup: false, /* 팝업을 통한 연동처리 여부 */
-			loginButton: {color: "green", type: 3, height: 60} /* 로그인 버튼의 타입을 지정 */
-		}
-	);
-
-	/* 설정정보를 초기화하고 연동을 준비 */
-	naverLogin.init();
-
-/* 현재 로그인 상태를 확인 */
-  window.addEventListener('load', function () {
-  naverLogin.getLoginStatus(function (status) {
-  if (status) {
-  /* 로그인 상태가 "true" 인 경우 로그인 버튼을 없애고 사용자 정보를 출력합니다. */
-  setLoginStatus();
-  }
-  });
-  });
+  var naver_id_login = new naver_id_login("txJsAHBUQ68ptqMzm_5I", "http://localhost/team_project/page/login/callback.php");
+  var state = naver_id_login.getUniqState();
+  naver_id_login.setButton("white", 2,40);
+  naver_id_login.setDomain("http://localhost/team_project/");
+  naver_id_login.setState(state);
+  // naver_id_login.setPopup(false);
+  naver_id_login.init_naver_id_login();
 </script>
   	</div>
   </div>
