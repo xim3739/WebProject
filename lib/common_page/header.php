39c4893335fee6be7eb6052a20d17623c8e6cb13
jms1132@naver.com <54921451+jms1132@users.noreply.github.com>
2020-02-24 15:17:39 +0900

    header 로그인 세션 값 반영/메인에 로그인 연결

--- lib/common_page/header.php
+++ lib/common_page/header.php
@@ -8,9 +8,39 @@
           <input type="submit" value="  " id="btn_search">
         </form>
       </div>
+
+      <?php
+            @session_start();
+            if (isset($_SESSION["userid"])) {
+                $userid = $_SESSION["userid"];
+            } else {
+                $userid = "";
+            }
+            if (isset($_SESSION["username"])) {
+                $username = $_SESSION["username"];
+            } else {
+                $username = "";
+            }
+              echo ("<script>console.log(document.cookie)</script>;");
+      ?>
       <div id="icon_box">
-        <span id="btn_login" onclick="pop_up(this)"> </span>
-        <span id="btn_info" onclick="pop_up(this)"> </span>
+
+          <?php
+      if (!$username) {
+      ?>
+      <input type="button" class="btn btn-primary" value="Sign In" onclick="window.open('../login/login_and_signup.php','','width=500,height=700,left=300')">
+      <?php
+      } else {
+          $logged = $username."(".$userid.")님"; ?>
+            <span><?=$logged?> </span>
+            <span><a href="../../page/login/logout.php" onclick="naver_logout()">로그아웃</a></span>
+            <span> | </span>
+              <span><a href="#" onclick="naver_logout()">마이페이지</a></span>
+            <?php
+      }
+      ?>
+        </ul>
+
       </div>
     </div>
   </div>
@@ -24,7 +54,7 @@
     <li class="t_co13"><a href="">임시보호</a></li>
     <li class="t_co14"><a href="../../page/board/board_form.php">자유게시판</a></li>
     <li class="t_co15"><a href="">후원하기</a></li>
-  </ul> 
+  </ul>
 </div>
 <!-- <div id="pop_up" class="z_index2">
   <div id="pop_box" onmouseleave="pop_down()">
