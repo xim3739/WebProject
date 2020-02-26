c486e573efa7c665051e04e4109e6d5e989232bb
HanHyeonggyu <1hanho94@gmail.com>
2020-02-26 10:17:08 +0900

    hospital_list.js 얼럿삭제

--- js/index/hospital_list.js
+++ js/index/hospital_list.js
@@ -30,7 +30,7 @@ $(document).ready(function () {
                             "addr": addr
                         },
                         success: function (response) {
-                            alert("SUCCESS");
+                            
                         }
                     });
                 }
