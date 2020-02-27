3f38791b453c6bdf76973c8750d99aeb1452b6b4
tlawogus11106 <tlawogus11106@gmail.com>
2020-02-27 20:32:06 +0900

    메인 페이지 충돌 정리

--- page/main/main.php
+++ page/main/main.php
@@ -41,80 +41,7 @@
   <?php include "../../lib/common_page/header.php"; ?>
   <!-- Page Content -->
   <section style="margin-top : 90px">
-<<<<<<< HEAD
-  <?php
-    if(isset($_GET['category'])) {
 
-        $getCategory = $_GET['category'];
-
-        $sql = "SELECT * FROM `board` WHERE `category` = '$getCategory'";
-
-        $result = mysqli_query($connect, $sql);
-        $page_num = mysqli_num_rows($result);
-
-        if($page_num) {
-          for($i = 0; $i < $page_num; $i++) {
-            mysqli_data_seek($result, $i);
-
-            $row = mysqli_fetch_array($result);
-
-            $num = $row['num'];
-            $id = $row['id'];
-            $name = $row["name"];
-            $regist_day = $row["regist_day"];
-            $subject = $row["subject"];
-            $content = $row["content"];
-            $file_name = $row["file_name"];
-            $file_type = $row["file_type"];
-            $file_copied = $row["file_copied"];
-            $hit = $row["hit"];
-            $content = str_replace(" ", "&nbsp;", $content);
-            $content = str_replace("\n", "<br>", $content);
-
-      ?>
-        <div class="container">
-          <!-- Heading Row -->
-          <div class="row align-items-center my-5">
-            <div class="col-lg-7 no-flex" style="width: 420px; height: 186.66px; overflow : hidden;">
-            <?php
-              if ($file_name) {
-                $real_name = $file_copied;
-                $file_path = "../../data/".$real_name;
-                $file_size = filesize($file_path);
-              }else{
-                $file_path="../../img/board/default.jpg";
-              }
-            ?>
-              <img class="img-fluid rounded mb-4 mb-lg-0" src="<?=$file_path?>">
-            </div>
-            <div class="col-lg-5 no-flex">
-              <h1 class="font-weight-light"><?=$subject?></h1>
-              <p><?=$content?></p>
-              <a class="btn btn-primary" href="../../page/board/board_widen.php?num=<?=$num?>">Call to Action!</a>
-              <span class="reply"><img src="" alt="">조회수 : <?=$hit?></span>
-            </div>
-          </div>
-        </div>
-      <?php
-          }
-        } else {
-      ?>
-        <div class="container">
-          <!-- Heading Row -->
-          <div class="row align-items-center my-5">
-            <div class="col-lg-7 no-flex" style="width: 420px; height: 186.66px; overflow : hidden;">
-
-              <img class="img-fluid rounded mb-4 mb-lg-0" src="../../img/board/default.jpg">
-            </div>
-            <div class="col-lg-5 no-flex">
-              <h1 class="font-weight-light">게시글이 없습니다.</h1>
-              <p></p>
-              <span class="reply"><img src="" alt=""></span>
-            </div>
-          </div>
-        </div>
-      <?php
-=======
         <?php 
         if(isset($_GET['category'])){
           $category=$_GET['category'];
@@ -122,7 +49,7 @@
         }else{
 
           $sql="SELECT * FROM `board`";
->>>>>>> a5505168f0b894388e28ce13770b3ef6f1ebb2e1
+
         }
         $result=mysqli_query($connect,$sql);
         $page_num=mysqli_num_rows($result);
