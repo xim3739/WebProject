0c97d97062a8dc2671290df5335585cefdbc2d7e
HanHyeonggyu <1hanho94@gmail.com>
2020-02-29 22:54:46 +0900

    스크롤 중복 오류 수정

--- js/main/scroll.php
+++ js/main/scroll.php
@@ -3,6 +3,7 @@ $(document).ready(function () {
     console.log('<?=$_SESSION['userid']?>');
     let isEnd = false;
     var page=0;
+    var flag=true;
     $(function(){
         $(window).scroll(function(){
             let $window = $(this);
@@ -10,20 +11,26 @@ $(document).ready(function () {
             let windowHeight = $window.height();
             let documentHeight = $(document).height();
             
+            
            // console.log("documentHeight:" + documentHeight + " | scrollTop:" + scrollTop + " | windowHeight: " + windowHeight );
             
             // scrollbar의 thumb가 바닥 전 30px까지 도달 하면 리스트를 가져온다.
-            if( scrollTop + windowHeight + 10 > documentHeight ){
-                fetchList();
+            if(flag){
+              if( scrollTop + windowHeight + 30 > documentHeight ){
+                  fetchList();
+              }
+
             }
         })
         fetchList();
     })
     
     let fetchList = function(){
+        flag=false;
         if(isEnd == true){
             return;
         }
+        console.log("부르냐");
         <?php 
         if(isset($_GET['category'])){
             $cate=$_GET['category'];
@@ -45,13 +52,14 @@ $(document).ready(function () {
         // renderList 함수에서 html 코드를 보면 <li> 태그에 data-no 속성이 있는 것을 알 수 있다.
         // ajax에서는 data- 속성의 값을 가져오기 위해 data() 함수를 제공.
         let startNo = $("#list-guestbook li").last().data("no") || 0;
+        let check_duplicate=[];
         $.ajax({
             url:"../../db/main/main_item.php",
             type: "GET",
             data:{'page':page,'category':"<?=$cate?>","form":"<?=$form?>"},
              
             success: function(result){
-                //console.log(result);
+                setTimeout(function(){flag=true;},500);
                 // 컨트롤러에서 가져온 방명록 리스트는 result.data에 담겨오도록 했다.
                 // 남은 데이터가 5개 이하일 경우 무한 스크롤 종료
                 if(!result.includes('empty')){
@@ -67,8 +75,7 @@ $(document).ready(function () {
                         var file_path=response[i].file;
                         var num=response[i].num;
                         var id=response[i].id;
-                        console.log(file_path);
-
+                        
                         if('<?=$board?>'&&'<?=$form?>'){
                             var html=`
                             <div class="board_center">
@@ -86,7 +93,7 @@ $(document).ready(function () {
                         </div>
                       </div>
                       </div>`;
-
+           
                         }else{
                             var html=`
                             <div class="board_center">
@@ -133,10 +140,11 @@ $(document).ready(function () {
                         
                         $('section').append(html);
                     }
-                    console.log(page);
                     page+=5;
+                    console.log(page);
                     
                 }else{
+
                     isEnd = true
                 }
                 
