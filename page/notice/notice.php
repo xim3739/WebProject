b20c0c4fc935c4dd40c3099e59a6dd34a4dae65a
tlawogus11106 <tlawogus11106@gmail.com>
2020-03-02 15:05:30 +0900

    main 게시글 작성자 이름 추가

--- page/notice/notice.php
+++ page/notice/notice.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>찾아JOO</title>
 
     <script src="http://code.jquery.com/jquery-1.12.4.min.js"></script>
     <link href="../../css/main/bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -16,6 +16,7 @@
     <?php include "../../lib/common_page/header.php"; ?>
 
     <section id="section" style="margin-top: 48px;">
+    <div id="div_top_notice">
         <h1>안녕하세요 찾아JOO 입니다</h1>
         <br/>
         <ul>
@@ -26,7 +27,39 @@
             <li>
                 <em>찾아JOO</em>에서는 잃어버린 <em>반려견</em>, <em>반려묘</em>들을 찾을 수 있는 사이트 입니다.
             </li>
+            <hr>
         </ul>
+    </div>
+    <br>
+    <div id="div_middle_notice">
+        <ol>
+            <h4>반려견을 잃어버리지 않는 방법!</h4>
+            <li>
+                이름표를 달아줍니다. <br> 
+                반려견에게 이름표를 달아줍니다. 이름표에는 주인과 반려견의 이름, 전화번호 등을 간단히 써줍니다.
+            </li>
+            <hr>
+            <li>
+                외출 시에는 반드시 목줄을 맨다. <br> 
+                외출 시의 목줄은 분실방지뿐만 아니라 교통사고 등 불의의 사고를 예방해 주고 아무거나 주워 먹는 것을 막을 수도 있습니다.
+            </li>
+            <hr>
+            <li>
+                귀환 명령을 확실하게 가르칩니다. <br>
+                반려견에 있어서 '이리와', '기다려' 는 꼭 가르쳐야 할 명령어 입니다. 외부 활동에서 자칫 산만해지거나 혹은 흥미로운 일에 이끌려 보호자의 존재와 거리를 잊고 돌아다니다 보면 잃어버리는 일이 발생합니다. 보호자가 반려견의 행동을 제지하는 명령에 대해서는 반드시 숙지 시켜야합니다.
+            </li>
+            <hr>
+            <li>
+                반려견을 잃어버렸을 때의 행동요령을 미리 숙지 합니다. <br>
+                반려견을 일어버렸을 때 어떻게 할지 몰라서 중요한 시간을 허비하는 경우가 많이 있습니다. 미리 주변의 반려동물 보호센터, 동물 보호소 등의 연락처를 숙지해 두면 위급 상황에 즉시 대처가 가능하며 반려견의 최근 사진을 항상 가지고 있으면 찾는데 많은 도움이 됩니다.
+            </li>
+            <hr>
+        </ol>
+        <br>
+        <ol>
+            <h4></h4>
+        </ol>
+    </div>
         
     </section>
 
